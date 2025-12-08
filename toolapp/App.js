620f6bdbe45477fc47c9226e8cb6ffc69c6fa09a
ElJiva <angeljiva48@outlook.com>
2025-12-08 01:17:43 -0600

    App.py adjustments

--- toolapp/App.js
+++ toolapp/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-// Importamos el archivo de rutas que creamos en el paso anterior
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <>
-      {/* Barra de estado blanca/clara para fondo oscuro */}
+      {}
       <StatusBar style="light" />
-      {/* Cargamos el navegador que decide qué pantalla mostrar */}
+      {}
       <AppNavigator />
     </>
   );
